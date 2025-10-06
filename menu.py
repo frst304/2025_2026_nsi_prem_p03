01807b0fbd52a67ce3b6cc33c6ce82deaac2977d
frst_304 <130504630+frst304@users.noreply.github.com>
2025-10-06 13:02:45 +0200

    menu system

--- menu.py
+++ menu.py
@@ -1,8 +1,39 @@
-from login import login_fctn
+import sys
 import os
+
+sys.path.append(os.path.join(os.getcwd(), 'user_actions'))
+
+from login import login_fctn
+from view_sold import view_sold_fctn # type: ignore
+from pull_money import pull_money_fctn # type: ignore
+from add_money import add_money_fctn # type: ignore
 
 def menu():
     user = login_fctn()
     os.system('cls')
-    print(f'[------------------ Bienvenue {user['name']} ------------------]')
+    print(f"[------------------ Bienvenue {user['name']} ------------------]")
+    print("[1] Voir son solde  [2] Retirer de l'argent  [3] Ajouter de l'argent")
     print('')
+    
+    while True:
+        action = input('Choisissez une action (1, 2, ou 3) : ')
+
+        if action == "1":
+            os.system('cls')
+            view_sold_fctn()
+            break
+
+        elif action == "2":
+            os.system('cls')
+            pull_money_fctn()
+            break
+
+        elif action == "3":
+            os.system('cls')
+            add_money_fctn()
+            break
+        
+        else:
+            os.system('cls')
+            print("Option invalide. Veuillez choisir 1, 2 ou 3.")
+            menu()
