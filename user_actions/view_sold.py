e5b2a5033c83d4a15a272011814a0de22cac44c6
Haron <haron.elmounzil2@gmail.com>
2025-11-03 11:42:47 +0100

    fonction view sold

--- user_actions/view_sold.py
+++ user_actions/view_sold.py
@@ -1,3 +1,5 @@
-
 def view_sold_fctn(user):
-
+    print("\n=== CONSULTATION DU SOLDE ===")
+    print(f"Titulaire du compte : {user['name'].title()}")
+    print(f"Solde actuel : {user['balance_account']:.2f} €")
+    print("==============================\n")
